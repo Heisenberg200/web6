9c6b1d1ee96255981ea29160a1f95839a578d64c
Heisenberg200 <65282454+Heisenberg200@users.noreply.github.com>
2022-05-19 20:48:45 +0300

    SQL injection fixed

--- app/controllers/todos.rb
+++ app/controllers/todos.rb
@@ -17,3 +17,23 @@ class TodosController < HomeController
     render json: JSON.dump(todos.as_json)
   end
 end
+###################################################################################
+#                   Posible code improvements and fixes                           #                                                                               
+###################################################################################
+#                                                                                 #
+# todos = Todo.where("user_id = #{params['user_id'].to_value}")                   #
+###################################################################################
+###################################################################################
+# Here in order to avoid the SQL injection we are casting the user_id             #
+# parameter to an expected value, avoiding the SQL injection                      #
+###################################################################################
+###################################################################################
+# Aother possible fix is is by using positional handlers to sanitize tained str-  #
+# ings as in the example:                                                         #
+# todos = Todo.where("zip_code = ? AND quantity >= ?", entered_zip_code,          #
+# entered_zip_quantity).first                                                     #
+# In such case the first parameter is a SQL fragment with qwestion mark, and the  #
+# second and third parameter will replacethe question marks with the value of     #
+# variables.                                                                      #
+###################################################################################  
+    
